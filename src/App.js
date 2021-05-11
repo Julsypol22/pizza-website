a326edca4bbd3c4d1ba0c38c6db1f64e7fd2f558
Julsypol22 <julsypol@gmail.com>
2021-05-11 21:22:02 +0300

    my-project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter as Router } from 'react-router-dom';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData } from './components/Products/data';
+import { GlobalStyle } from './globalStyles';
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading='Choose your favorite' data={productData} />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Products/ProductsElements.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const ProductsContainer = styled.div ` 
+    width: 100vw;
+    min-height: 100vh;
+    padding: 5rem calc ((10vw - 1300px) /2);
+    background:#150f0f;
+    color:#fff;
+`;
+export const ProductsWrapper = styled.div ` 
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0 auto;
+`;
+export const ProductCard = styled.div ` 
+    margin: 0 2rem;
+    line-height: 2;
+    width:300px;
+`;
+export const ProductCard = styled.div ` 
+    margin: 0 2rem;
+    line-height: 2;
+    width:300px;
+`;
+
